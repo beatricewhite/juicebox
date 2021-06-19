c272fbe104b47d081dd5aaf8965d85a82709ad42
beatrice <beatricewhitee@gmail.com>
2021-06-19 13:06:37 -0700

    part 2 complete

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,14 @@
 //grabs the client with destructuring from the export in index.js
 const {
     client,
-    getAllUsers,
     createUser,
+    updateUser,
+    getAllUsers,
+    getUserById,
+    createPost,
+    updatePost,
+    getAllPosts,
+    getPostsByUser,
 } = require('./index')
 
 
@@ -12,6 +18,7 @@ async function dropTables() {
         console.log("Starting to drop tables ...");
         //drop all tables (*)
         await client.query(`
+            DROP TABLE IF EXISTS posts;
             DROP TABLE IF EXISTS users;
         `);
 
@@ -31,8 +38,19 @@ async function createTables() {
             CREATE TABLE users (
                 id SERIAL PRIMARY KEY,
                 username varchar(255) UNIQUE NOT NULL,
-                password varchar(255) NOT NULL
+                password varchar(255) NOT NULL,
+                name varchar(255) NOT NULL,
+                location varchar(255) NOT NULL,
+                active boolean DEFAULT true
             );
+
+            CREATE TABLE posts(
+                id SERIAL PRIMARY KEY,
+                "authorId" INTEGER REFERENCES users(id),
+                title varchar(255) NOT NULL,
+                content TEXT NOT NULL,
+                active BOOLEAN DEFAULT true
+            )
         `);
 
         console.log("Finished building tables!")
@@ -47,16 +65,58 @@ async function createInitialUsers() {
     try {
         console.log("Starting to create users...");
 
-        const albert = await createUser({ username: 'albert', password: 'bertie99'});
-        const sandra = await createUser({ username: 'sandra', password: '2sandy4me'});
-        const glamgal = await createUser({ username: 'glamgal', password: 'soglam'});
+        await createUser({ 
+            username: 'albert', 
+            password: 'bertie99', 
+            name: 'Al Bert', 
+            location: 'Sidney, Australia'
+        });
+        await createUser({ 
+            username: 'sandra', 
+            password: '2sandy4me', 
+            name: 'Just Sandra', 
+            location: 'Ain\'t tellin\''
+        });
+        await createUser({ 
+            username: 'glamgal', 
+            password: 'soglam', 
+            name: 'Joshua', 
+            location: 'Upper East Side'
+        });
         
-
-        console.log(albert);
 
         console.log("Finished creating users!");
     } catch(error) {
         console.error("Error creating users!");
+        throw error;
+    }
+}
+
+async function createInitialPosts() {
+    try {
+        const [albert, sandra, glamgal] = await getAllUsers();
+
+        console.log("Starting to create posts ...");
+        await createPost({
+            authorId: albert.id,
+            title: "First Post",
+            content: "This is my first post I hope I love writing blogs as much as I love writing them."
+        });
+
+        await createPost({
+            authorId: sandra.id,
+            title: "How does this work?",
+            content: "Seriously, does this even do anything?"
+        });
+
+        await createPost({
+            authorId: glamgal.id,
+            title: "Living the Glam Life",
+            content: "Do you even? I swear that half of you are posing."
+        });
+        console.log("finished creating posts!")
+    } catch(error) {
+        console.log("Error creating posts!")
         throw error;
     }
 }
@@ -69,8 +129,10 @@ async function rebuildDB() {
         await dropTables();
         await createTables();
         await createInitialUsers();
+        await createInitialPosts();
 
     } catch (error) {
+        console.log("Error during rebuildDB")
         throw error;
     } //REMOVED FINALLY CLIENT END
 }
@@ -86,8 +148,31 @@ async function testDB() {
         // change const results and console log to rows so we can access rows field
         // const { rows } = await client.query(`SELECT * FROM users;`);
         //Now that line above is in index file we can remove and use const users
+        console.log("Calling getAllUsers");
         const users = await getAllUsers();
-        console.log("getAllUsers:", users);
+        console.log("Result:", users);
+
+        console.log("Calling updateUser on users[0]");
+        const updateUserResult = await updateUser(users[0].id, {
+            name: "Newname Sogood",
+            location: "Lesterville, KY"
+        });
+        console.log("Result:", updateUserResult);
+
+        console.log("Calling getAllPosts");
+        const posts = await getAllPosts();
+        console.log("Result:", posts);
+
+        console.log("Calling updatePost on posts[0]");
+        const updatePostResult = await updatePost(posts[0].id, {
+            title: "New Title",
+            content: "Updated Content"
+        });
+        console.log("Result:", updatePostResult);
+
+        console.log("Calling getUserById with 1");
+        const albert = await getUserById(1);
+        console.log("Result:", albert)
 
         console.log("Finished database tests!");
     } catch (error) {
