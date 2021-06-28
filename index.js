75956af1aa67edd7a5e3904ea021ca05347b873c
beatrice <beatricewhitee@gmail.com>
2021-06-27 22:28:07 -0500

    part 3 completed

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ server.listen(PORT, () => {
     console.log('The server is up on port', PORT)
 })
 
+// server.get('/background/:color', (req, res, next) => {
+//     res.send (`
+//         <body style="background: ${ req.params.color };">
+//             <h1>Hello World</h1>
+//         </body>
+//     `);
+// })
+
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
 //any time a requrest is made to a path starting with /api the apiRouter
